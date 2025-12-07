e58a4d952db8f90a9024b2182ff6739bd0576558
luciano <luchorichi123@gmail.com>
2025-12-07 11:56:59 -0300

    pruebas agregadas

--- src/lexer/semanticActions/AS3.java
+++ src/lexer/semanticActions/AS3.java
@@ -17,14 +17,14 @@ public class AS3 implements SemanticAction {
 
             // Chequeo de rango (16 bits)
             if (value < Short.MIN_VALUE || value > Short.MAX_VALUE) {
-                context.setToken(TokenType.ERROR, "Error en línea " + context.getLine() +
+                context.setToken(TokenType.ERROR, "Error Lexico en línea " + context.getLine() +
                         ": entero fuera de rango -> " + value);
             } else {
                 SymbolEntry entry = new SymbolEntry(lexeme, "int", value);
                 context.setToken(TokenType.INT16, entry); // <-- ¡ARREGLADO!
             }
         } catch (NumberFormatException e) {
-            context.setToken(TokenType.ERROR, "Error en línea " + context.getLine() +
+            context.setToken(TokenType.ERROR, "Error Lexico en línea " + context.getLine() +
                     ": formato inválido para entero -> " + lexeme);
         }
 
